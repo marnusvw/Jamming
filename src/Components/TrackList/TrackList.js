525ef0f7b974a8795e98821f8dbbb7032cb7d213
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-15 15:41:55 +0200

    Implement the TrackList component.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,1 +1,22 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+
+import Track from "../Track/Track";
+
+const TrackList = (props) => {
+  return (
+    <div className="TrackList">
+      {props.tracks.map((track) => {
+        return (
+            <Track
+          track={track}
+          key={track.id}
+          onAdd={props.onAdd}
+          isRemoval={props.isRemoval}
+          onRemove={props.onRemove}
+        />
+        )
+      })}
+    </div>
+  );
+};
+export default TrackList;
