10e4df5aa5f48bfed7b83e87df01f69497453f43
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-15 14:32:27 +0200

    Implement the SearchBar component.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,25 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import "./App.css";
 
+import Spotify from "../../util/Spotify"
+import SearchBar from '../SearchBar/SearchBar'
+
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [playlistName, setPlaylistName] = useState('New Playlist');
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+
+  const search =  useCallback(async (term) => {
+    const trackArray = await Spotify.search(term)
+    setSearchResults(trackArray)
+  });
+  
   return (
     <div className="App">
-     
+      <SearchBar onSearch={search}/>
+      <ul>
+        {searchResults.map(track => <li>{track.name}</li>)}
+      </ul>
     </div>
   );
 }
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,1 +1,21 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
+
+const SearcBar = (props) => {
+    const [term, setTerm] = useState('')
+
+    const handleTextChange = useCallback((event) => {
+        setTerm(event.target.value)
+    }, []);
+
+    const search = useCallback(() => {
+        props.onSearch(term)
+    }, [props.onSearch, term])
+
+    return (
+        <div>
+            <input placeholder="Enter a Song Title" value={term} type="text" onChange={handleTextChange}></input>
+            <button onClick={search}>Search</button>
+        </div>
+    )
+}
+export default SearcBar;
