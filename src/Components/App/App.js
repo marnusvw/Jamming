7085a1e5705444722f484f6625638a62b2f4af84
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-15 15:41:44 +0200

    Implement SearchResults component.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Spotify from "../../util/Spotify"
 import SearchBar from '../SearchBar/SearchBar'
+import SearchResults from '../SearchResults/SearchResults'
 
 function App() {
   const [searchResults, setSearchResults] = useState([]);
@@ -14,12 +15,19 @@ function App() {
     setSearchResults(trackArray)
   });
   
+  const addTrack = useCallback((track) => {
+    if (playlistTracks.some((savedTrack) => savedTrack === track.id))
+      return;
+
+    setPlaylistTracks((prevTracks) => [...prevTracks, track])
+  }, [])
+
   return (
     <div className="App">
       <SearchBar onSearch={search}/>
-      <ul>
-        {searchResults.map(track => <li>{track.name}</li>)}
-      </ul>
+      <div>
+      <SearchResults searchResults={searchResults} onAdd={addTrack}/>
+      </div>
     </div>
   );
 }
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,1 +1,13 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+
+import TrackList from "../TrackList/TrackList";
+
+const SearchResults = (props) => {
+  return (
+    <div className="SearchResults">
+      {props.searchResults.length > 0 && <h2>Results</h2>}
+      <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+    </div>
+  );
+};
+export default SearchResults;
