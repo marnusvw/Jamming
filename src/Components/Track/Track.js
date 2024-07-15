5daf669706987449b3413e55b790f8b27b79701a
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-15 15:42:07 +0200

    Implement the Track component.

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,1 +1,15 @@
 import React, {useState, useEffect} from "react";
+
+const Tracks = (props) => {
+    const onAdd = () => {
+        props.onAdd(props.track)
+    }
+    return (
+        <div>
+            <h3>{props.track.name}</h3>
+            <p>{props.track.artist} | {props.track.album}</p>
+        </div>
+    )
+}
+
+export default Tracks;
