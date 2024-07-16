0065015e072480c0011108bcc9df859f2ec602ce
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-16 14:30:02 +0200

    Cleaned up unsused imports and formatted the component.

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,31 +1,42 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useCallback } from "react";
+
+import "./Track.css";
 
 const Tracks = (props) => {
-    const onAdd = useCallback(() => {
-        props.onAdd(props.track)
-    }, [props.onAdd, props.track])
-    const onRemove = useCallback(() =>{
-        props.onRemove(props.track)
-    }, [props.onRemove, props.track])
-    const renderAction = () => {
-        if (props.isRemoval){
-            return (
-                <button onClick={onRemove}>Delete</button>
-            )
-        }
-        else {
-            return (
-                <button onClick={onAdd}>Add</button>
-            )
-        }
-    }
-    return (
+  const onAdd = useCallback(() => {
+    props.onAdd(props.track);
+  }, [props.onAdd, props.track]);
+  const onRemove = useCallback(() => {
+    props.onRemove(props.track);
+  }, [props.onRemove, props.track]);
+  const renderAction = () => {
+    if (props.isRemoval) {
+      return (
+        <button className="removeBtn" onClick={onRemove}>
+          Delete
+        </button>
+      );
+    } else {
+      return (
         <div>
-            <h3>{props.track.name}</h3>
-            <p>{props.track.artist} | {props.track.album}</p>
-            {renderAction()}
+          <button className="addBtn" onClick={onAdd}>
+            +
+          </button>
         </div>
-    )
-}
+      );
+    }
+  };
+  return (
+    <div className="trackCard">
+      <div className="trackInfo">
+        <h3>{props.track.name}</h3>
+        <p>
+          {props.track.artist} | {props.track.album}
+        </p>
+      </div>
+      {renderAction()}
+    </div>
+  );
+};
 
 export default Tracks;
