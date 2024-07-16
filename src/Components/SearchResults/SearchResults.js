be2635e395e3a2f3826a356aab20fe79ddcc1896
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-16 11:35:31 +0200

    Implement styles for the SearchResults component.

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-
+import './SearchResults.css'
 import TrackList from "../TrackList/TrackList";
 
 const SearchResults = (props) => {
   return (
-    <div className="SearchResults">
+    <div className="searchResults">
       {props.searchResults.length > 0 && <h2>Results</h2>}
       <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
     </div>
