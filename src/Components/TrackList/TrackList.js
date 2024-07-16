3130e31a74fdb348bc5c5e114e678b65c42dc96c
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-16 14:31:10 +0200

    Import the TrackList stylesheet and format component.

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,20 +1,20 @@
 import React from "react";
-
+import "./TrackList.css";
 import Track from "../Track/Track";
 
 const TrackList = (props) => {
   return (
-    <div className="TrackList">
+    <div className="trackList">
       {props.tracks.map((track) => {
         return (
-            <Track
-          track={track}
-          key={track.id}
-          onAdd={props.onAdd}
-          isRemoval={props.isRemoval}
-          onRemove={props.onRemove}
-        />
-        )
+          <Track
+            track={track}
+            key={track.id}
+            onAdd={props.onAdd}
+            isRemoval={props.isRemoval}
+            onRemove={props.onRemove}
+          />
+        );
       })}
     </div>
   );
